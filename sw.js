551015715388543fb2861a15268175c9ad1e312c
lilly <lillymollymail@gmail.com>
2026-05-18 18:37:17 +0600

    Initial commit

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'mbank-pwa-v2';
+const BASE = 'https://normmyme.github.io/afikmoofopf/';
+
+const FILES = [
+  BASE + 'payment.html',
+  BASE + 'manifest.json',
+  BASE + 'icon-192.png',
+  BASE + 'icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
